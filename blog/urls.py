5cd66378b8778e05a52cd46874d3183de5461dce
Mark Varabyou <vorobyovmark@gmail.com>
2017-11-28 16:40:50 -0600

    view templates

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,14 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
+    # ex: /
     url(r'^$', views.index, name='index'),
+    # ex: /5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
